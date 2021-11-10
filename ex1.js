e1fd3b8e51901f8cb212a8ec9c97828308c2751b
ferreiraferraz <gustavo@qsalto.com.br>
2021-11-09 22:47:19 -0300

    Fiquei nervoso e não consegui realizar

---
+++ ex1.js
@@ -0,0 +1,16 @@
+let pipoca = 10;
+let macarrao = 8;
+let carne = 15;
+let feijao = 12;
+let brigadeiro = 8;
+let tempo = 0;
+const fazerpipoca = (x,tempo) => pipoca * tempo;
+        if (fazerpipoca > 20) {
+            console.log("A comida queimou");
+        } else if (fazerpipoca < 10)
+            console.log("tempo insuficiente");
+            else if (fazerpipoca > 30)
+            console.log("kabumm");
+fazerpipoca(pipoca, 10);
+        
+        
