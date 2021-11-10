ebc31fae196f8eddedd5042af5c94fb883a34784
ferreiraferraz <gustavo@qsalto.com.br>
2021-11-09 23:51:05 -0300

    Quase que não saia

--- ex1.js
+++ ex1.js
@@ -1,16 +1,54 @@
-let pipoca = 10;
-let macarrao = 8;
-let carne = 15;
-let feijao = 12;
-let brigadeiro = 8;
-let tempo = 0;
-const fazerpipoca = (x,tempo) => pipoca * tempo;
-        if (fazerpipoca > 20) {
-            console.log("A comida queimou");
-        } else if (fazerpipoca < 10)
-            console.log("tempo insuficiente");
-            else if (fazerpipoca > 30)
-            console.log("kabumm");
-fazerpipoca(pipoca, 10);
-        
-        
+const fazerpipoca = (tempo) => {
+        switch(tempo) {
+            case 1: case 2 : case 3 : case 4 : case 5 : case 6 : case 7 :case 8 : case 9 :    
+                return 'Tempo insuficiente';
+            case 10:
+                return 'Prato pronto, bom apetite!!!';
+                    case 20 :
+                    return 'A comida queimou';
+                    case 30 : 
+                    return 'kabumm';
+            default : 
+            return 'Prato inexistente';
+    }
+}
+console.log(fazerpipoca(30));
+
+const macarrao = (tempo) => {
+    switch(tempo) {
+        case 1: case 2 : case 3 : case 4 : case 5 : case 6 : case 7 :    
+            return 'Tempo insuficiente';
+        case 8:
+            return 'Prato pronto, bom apetite!!!';
+                case 16 :
+                return 'A comida queimou';
+                case 24 : 
+                return 'kabumm';
+        default : 
+        return 'Prato inexistente';
+}   
+}
+console.log(macarrao(24));
+
+const carne = (tempo) => {
+    switch(tempo) {
+        case 1: case 2 : case 3 : case 4 : case 5 : case 6 : case 7 : case 7 :case 8 :
+        case 9 :case 10 :case 11 :case 12 :case 13 : case 14 
+            return 'Tempo insuficiente';
+        case 8:
+            return 'Prato pronto, bom apetite!!!';
+                case 16 :
+                return 'A comida queimou';
+                case 24 : 
+                return 'kabumm';
+        default : 
+        return 'Prato inexistente';
+}   
+}
+console.log(macarrao(24));
+
+
+
+
+
+console.log('Prato pronto, bom apetite!!!');
